4234904795cc1f946c2e3ac3fa02a5de91b62eac
44178 <71744417+44178@users.noreply.github.com>
2024-08-25 21:29:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -254,7 +254,7 @@ const BLOG = {
   MOUSE_FOLLOW: process.env.NEXT_PUBLIC_MOUSE_FOLLOW || true, // 开关
   // 这两个只有在鼠标跟随特效开启时才生效
   // 鼠标类型 1：路劲散点 2：下降散点 3：上升散点 4：边缘向鼠标移动散点 5：跟踪转圈散点 6：路径线条 7：聚集散点 8：聚集网格 9：移动网格 10：上升粒子 11：转圈随机颜色粒子 12：圆锥放射跟随蓝色粒子
-  MOUSE_FOLLOW_EFFECT_TYPE: 6, // 1-12
+  MOUSE_FOLLOW_EFFECT_TYPE: 12, // 1-12
   MOUSE_FOLLOW_EFFECT_COLOR: '#ef672a', // 鼠标点击特效颜色 #xxxxxx 或者 rgba(r,g,b,a)
 
   // 樱花飘落特效
@@ -311,7 +311,7 @@ const BLOG = {
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://https://music.163.com/outchain/player?type=2&id=731419&auto=1&height=66%22%3E%3C/iframe%3E',
+      url: 'http://music.163.com/song/media/outer/url?id=29723028.mp3',
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
