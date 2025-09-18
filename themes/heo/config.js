bfce21c587b3d1d0b740d315c09155fa74c632f8
44178 <71744417+44178@users.noreply.github.com>
2025-09-19 00:22:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '记录分享',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'ETHAI.TOP',
+  HEO_HERO_TITLE_3: 'WWW.ETHAI.TOP',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '44-178X 欢迎来到我的博客',
   HEO_HERO_TITLE_LINK: 'https://www.ethai.top',
